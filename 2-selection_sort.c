2a79f00795af602a37d01770f35492cbb07128e4
aallm004 <abigailrosea10@gmail.com>
2024-04-08 22:06:32 +0000

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,5 +9,30 @@
 */
 void selection_sort(int *array, size_t size)
 {
+	size_t a, b, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+	return;
+
+	a = 0
+	while (a < size - 1)
+	{
+		min = a;
 	
+		for (b = i + 1; b < size; b++)
+		{
+			if (array[b] < array[min])
+			min = b;
+		}
+
+		if (min != i)
+		{
+			temp = array[a];
+			array[a] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+		a++
+	}
 }
