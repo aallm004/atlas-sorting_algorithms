fe8f081a3065e35015cc2ac0a502ffbc1edbdc87
aallm004 <abigailrosea10@gmail.com>
2024-04-08 21:54:34 +0000

    first push insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,5 +8,24 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_t *actual, *change, *prior;
+
+	if (!list || !*list)
+		return;
+
+	actual = *list;
+	while ((actual = actual->next))
+	{
+		change = actual;
+		while (change->prev && change->n < change->prev->n)
+		{
+			prior = change->prev;
+			if (change->next)
+				change->next->prev = prior;
+			if (prior->prev)
+				prior->prev->next = change;
+
+			print_list(*list);
+		}
+	}
 }
